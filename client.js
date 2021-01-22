6be5a583c5905530ffc25ce03d9ed97dd90e7c04
zhangtj <1771988873@qq.com>
2021-01-22 23:22:16 +0800

    修正代码格式

--- client.js
+++ client.js
@@ -4,12 +4,12 @@ const Websocket = require('ws');
 const wss = new Websocket('ws://127.0.0.1:8000');
 
 wss.on('open', function () {
-  for(let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     wss.send("hello from client: " + i);
   }
-  
+
 })
 
-wss.on('message', function(msg) {
+wss.on('message', function (msg) {
   console.log(msg);
 })
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Websoket server
-const  Websocket = require("ws");
+const Websocket = require("ws");
 
 const wss = new Websocket.Server({
   port: 8000
 });
 
-wss.on('connection', function(ws){
-  ws.on('message', function(msg){
+wss.on('connection', function (ws) {
+  ws.on('message', function (msg) {
     console.log(msg);
   })
   ws.send("Message from server!");
